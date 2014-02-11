a66d8365fd291b4245bbcb5601c9830c8ff0f170
hongkedavid <hongkedavid@gmail.com>
2014-02-11 18:24:49 +0800

    Delete find.sh

--- find.sh
+++
@@ -1,3 +0,0 @@
-# this simple script searches for a list of files (say *.c files) containing a special string as input argument recursively 
-# run by "./find.sh 'special string'"
-find . -type f -name "*.c" -exec grep -ls $1 {} \;
